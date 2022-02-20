1ed2386a8ba3e8fd97b37e0c80377f81d9b3f52a
Anson Cheung <anson@Ansons-MacBook-Pro.local>
2022-02-20 19:22:25 +0000

    feat: add lorem pisum

--- components/SpaceCard.js
+++ components/SpaceCard.js
@@ -3,9 +3,6 @@
 // @param { Array<String> } list of names
 // @param { String } button text
 // @param { Function } buttonActio / callback
-
-import { LockClosedIcon } from "@heroicons/react/solid";
-
 //
 const SpaceCard = ({space, restrictedAreas, buttonAction}) => {
 
@@ -14,7 +11,7 @@ const SpaceCard = ({space, restrictedAreas, buttonAction}) => {
     }
 
     return (
-        <div onClick={() => buttonAction(space) } className="cursor-pointer hover:hover:drop-shadow-2xl text-lit-100 bg-white overflow-hidden text-xs rounded-xl">
+        <div onClick={() => buttonAction(space) } className="cursor-pointer border-4 border-lit-100 hover:border-lit-400 transition hover:drop-shadow-2xl text-lit-100 bg-white overflow-hidden text-xs rounded-xl">
 
             <div className="h-48 flex justify-center items-center overflow-hidden">
                 <img className="object-cover w-full h-full" src="https://picsum.photos/seed/picsum/1000/1000"/>
@@ -34,26 +31,17 @@ const SpaceCard = ({space, restrictedAreas, buttonAction}) => {
                         </div>
                     </>
                     :<>
-                        <ul className="h-28 overflow-auto mt-6 w-full text-lit-900">
+                        <ul className="h-30 overflow-auto mt-6 w-full text-lit-900">
                             <div className="m-auto mb-1 text-lg text-left">
                                 Restricted Rooms
                             </div>
                             {
                                 restrictedAreas.map((area) => {
                                     return (
-                                        // <li className="h-10 flex bg-lit-500 p-1 mb-1 rounded-lg">
-                                             
-                                        //     <div className="w-24 flex justify-center">
-                                        //     <span className="m-auto capitalize">{ area.name }</span>
-                                        //     </div>
-                                        //     <div className="text-left w-full flex">
-                                        //         <span className="mt-auto mb-auto">{ area.humanised }</span>
-                                        //     </div>
-                                        // </li>
                                         <li className="mb-2 pb-1 border-b flex">
                                             <div className="flex justify-center">
                                                 <div className="m-auto w-6">
-                                                    <LockClosedIcon className="m-auto"/>
+                                                    <img className="m-auto" src="/lit-logo.png" />
                                                 </div>
                                             </div>
                                             <div className="ml-2 text-left">
