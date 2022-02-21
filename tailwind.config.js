0fe7f0500b77d22105ee87ea86fee7fdae148176
Anson Cheung <anson@Ansons-MacBook-Pro.local>
2022-02-21 21:05:15 +0000

    fix: css background

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage : {
-        'main': "url('../public/background.png')",
+        'main': "url('/background.png')",
       },
       colors: {
         'lit': {
