a7c941883f791c2b1de6ff3d360b474e3401d779
Anson Cheung <anson@Ansons-MacBook-Pro.local>
2022-02-23 22:39:23 +0000

    fix: remove unused env

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ module.exports = {
     domains: ['litprotocol.com', 'gather.town'],
   },
   publicRuntimeConfig: {
-    BACKEND_API: process.env.REACT_APP_LIT_GATEWAY_FRONTEND_API_URL,
-    HOST: process.env.REACT_APP_LIT_GATEWAY_FRONTEND_HOST,
-    ENV: process.env.REACT_APP_LIT_GATEWAY_ENVIRONMENT,
-    LIT_OG_NFT_TOKEN_ADDRESS: process.env.REACT_APP_LIT_GATEWAY_LIT_OG_NFT_TOKEN_ADDRESS,
-    REACT_APP_LIT_GATEWAY_DISCORD_CLIENT_ID: process.env.REACT_APP_LIT_GATEWAY_DISCORD_CLIENT_ID,
+    BACKEND_API: process.env.REACT_APP_LIT_GATEWAY_FRONTEND_API_URL
   }
 }
