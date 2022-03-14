112b237506f3341bac03d85cd81f4384d2ae1af5
Anson Cheung <anson@Ansons-MacBook-Pro.local>
2022-03-14 22:44:04 +0000

    fix: logo ui

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -1,6 +1,5 @@
 import BtnConnectWallet from "../BtnConnectWallet";
-import { LitLogo } from "@websaam/ui";
-
+import LitLogo from "../Ui/LitLogo";
 import React from 'react'
 import { InfoBox } from "../InfoBox";
 import { useAppContext } from "../../state/AppProvider";
@@ -15,14 +14,7 @@ export default function LayoutHeader({subtitle}){
     return (
         <header className="sticky top-0 w-full bg-lit-dark relative mx-auto flex justify-between items-center h-16 py-9 px-6 z-10">
             <div className="relative">
-                <Link href="/">
-                    <LitLogo
-                        cursorPointer
-                        onClick={() => {}}
-                        subtitle="Gather Spaces"
-                        title="Lit Protocol"
-                    />
-                </Link>
+                <LitLogo/>
             </div>
 
             {/* Nav: Links */}
