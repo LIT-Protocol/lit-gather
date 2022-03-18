493b4d1df2efbf51a942e2c04c3214b6f556abad
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2022-03-18 01:47:32 -0400

    add bugsnag for error tracking

--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,18 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
-    render() {
-      return (
-        <Html>
-          <Head>
-            {/* Global Site Tag (gtag.js) - Google Analytics */}
-            <script
-              async
-              src={`https://www.googletagmanager.com/gtag/js?id=G-10K11J7XGS`}
-            />
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=G-10K11J7XGS`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
@@ -20,14 +20,22 @@ export default class MyDocument extends Document {
                         page_path: window.location.pathname,
                     });
                 `,
-              }}
-            />
-          </Head>
-          <body>
-            <Main />
-            <NextScript />
-          </body>
-        </Html>
-      )
-    }
+            }}
+          />
+          <script src="//d2wy8f7a9ursnm.cloudfront.net/v7/bugsnag.min.js"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                Bugsnag.start({ apiKey: 'de6a70577c09320662365bca4488a0e2' })
+                `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
+}
