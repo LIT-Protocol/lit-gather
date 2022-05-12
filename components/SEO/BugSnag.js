9c66d29969a8b51b1ce007500fabd1ba0a68f987
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2022-05-12 13:45:10 -0700

    no async bugsnag to fix console error

--- components/SEO/BugSnag.js
+++ components/SEO/BugSnag.js
@@ -1,19 +1,16 @@
-const BugSnag = ({api}) => {
-    return (
-        <>
-        <script 
-            async
-            src="https://d2wy8f7a9ursnm.cloudfront.net/v7/bugsnag.min.js" 
-        />
-        <script
-            dangerouslySetInnerHTML={{
-            __html: `
+const BugSnag = ({ api }) => {
+  return (
+    <>
+      <script src="https://d2wy8f7a9ursnm.cloudfront.net/v7/bugsnag.min.js" />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
                 Bugsnag.start({ apiKey: '${api}' })
                 `,
-            }}
-        />
-        </>
-    );
-}
+        }}
+      />
+    </>
+  );
+};
 
 export default BugSnag;
